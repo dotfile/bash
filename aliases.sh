0608fcbdfdf09daf942c0f7910799c8c8d87c863
Brandon Thomas Suit <brandon.suit@gmail.com>
2011-08-19 21:03:18 -0400

    First commit

---
+++ aliases.sh
@@ -0,0 +1,14 @@
+# vim: set filetype=bash :
+# Bash aliases (~/.bash/aliases.sh)
+# Add to .bashrc, `. ~/.bash/thisfile.sh`
+
+# Programs
+alias ack='ack-grep'
+
+# Typofix
+alias clea='clear'
+alias clera='clear'
+
+# Reminders
+alias quit='echo "No, exit."'
+
